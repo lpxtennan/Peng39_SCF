c78c9fa38a0f2c35b3f424d6ec5d65a10b0930b7
1687219868 <1687219868@qq.com>
2021-03-14 00:43:12 +0800

    提交文件

---
+++ tscf.js
@@ -0,0 +1,22 @@
+"use strict";
+
+// see https://github.com/serverless/serverless/blob/master/README_CN.md#how-to-install-a-service
+
+exports.main_handler = (event, context, callback) => {
+  let argv = process.argv;
+  argv[2] = process.env.cmd;
+  argv[3] = "--user";
+  argv[4] = process.env.user;
+  argv[5] = "--password";
+  argv[6] = process.env.password;
+  argv[7] = "--appid"
+  argv[8] = process.env.appid;
+  console.log(argv.join(" "));
+  prcess.env.tasks=1;
+  if (!process.env.tasks) {
+    callback(null, "必须单独指定任务");
+  } else {
+    require("./AutoSignMachine.js").run(argv);
+    callback(null, "触发任务执行成功");
+  }
+};
